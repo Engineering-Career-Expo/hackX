3a06b1d810a479a42a3068f1c06a82d1e9b2373c
bamiogunfemi <44452615+bamiogunfemi@users.noreply.github.com>
2020-05-01 01:39:43 +0100

    Revert "Backend"

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # hackX
-
-Run npm install
--- olawale1rty/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://Ecx:unilag-ecx@hackx-ykqen.mongodb.net/test?retryWrites=true&w=majority', {useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true})
-	.then(()=> console.log("Database Connected Successfully"))
-	.catch(err=>console.log(err));
-	
-
-
-
