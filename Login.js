848fe2941a5602889047e75c55410d2e7e39ab4a
Omotayewo <60750250+Omotayewo@users.noreply.github.com>
2020-05-03 05:25:28 +0100

    Update Login.js

--- Login.js
+++ Login.js
@@ -12,16 +12,3 @@ function viewPassword() {
     passStatus.className='fa fa-eye-slash';
   }
 }
-var myham = document.getElementsByClassName("menu");
-var second = document.getElementsByClassName("list");
-
-
-myham[0].addEventListener("click",myfunc1)
-var a = 0;
-function myfunc1(){
-
-second[0].style.height ="270px"		
-second[0].style.width ="auto"				
-	
-a++;
-}
