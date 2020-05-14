51f0b58f00a7a96b259e4c48bfa6ab9dcc8ae3e0
Arafah Ogunfemi <ayobamiarafat09@gmail.com>
2020-05-14 17:00:25 +0100

    tweaks

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-$(function () {
-  var includes = $("[data-include]");
-  jQuery.each(includes, function () {
-    var file = "../components/" + $(this).data("include") + ".html";
-    $(this).load(file);
-  });
-});
+// $(function () {
+//   var includes = $("[data-include]");
+//   jQuery.each(includes, function () {
+//     var file = "../components/" + $(this).data("include") + ".html";
+//     $(this).load(file);
+//   });
+// });
 
 //nav
 
