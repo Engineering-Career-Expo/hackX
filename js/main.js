2aa13c5bcaafea6ec715c8d42868731ea9e0e469
Arafah Ogunfemi <ayobamiarafat09@gmail.com>
2020-05-15 21:11:52 +0100

    reup

--- js/main.js
+++ js/main.js
@@ -1,13 +1,6 @@
-// $(function () {
-//   var includes = $("[data-include]");
-//   jQuery.each(includes, function () {
-//     var file = "../components/" + $(this).data("include") + ".html";
-//     $(this).load(file);
-//   });
-// });
+
 
 //nav
-
 var myham = document.getElementsByClassName("myham");
 var second = document.getElementsByClassName("nav-second");
 var reg1 = document.getElementsByClassName("nav-reg1");
@@ -16,10 +9,10 @@ myham[0].addEventListener("click", myfunc1);
 var a = 0;
 function myfunc1() {
   reg1[0].style.opacity = "0";
+
   second[0].style.height = "325px";
   second[0].style.width = "auto";
   myham[0].innerHTML = "X";
-  s;
   a++;
 
   if (a % 2 === 0) {
