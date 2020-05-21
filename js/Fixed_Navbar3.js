cabb6091a32f235b04d554d867a0a2a9a36f41a4
unknown <oluwatoyosiadeleye4@gmail.com>
2020-05-21 21:01:02 +0100

    fixed-about4

--- js/Fixed_Navbar3.js
+++ js/Fixed_Navbar3.js
@@ -2,22 +2,20 @@ var myham = document.getElementsByClassName("navbar_myham");
 var second = document.getElementsByClassName("navbar_second");
 var reg1 = document.getElementsByClassName("navbar_reg1");
 
-
-myham[0].addEventListener("click",myfunc1)
+myham[0].addEventListener("click", myfunc1);
 var a = 0;
-function myfunc1(){
-reg1[0].style.opacity ="0";
+function myfunc1() {
+  reg1[0].style.opacity = "0";
 
-second[0].style.height ="325px"		
-second[0].style.width ="auto"				
-myham[0].innerHTML ="X"	
-a++;
+  second[0].style.height = "325px";
+  second[0].style.width = "auto";
+  myham[0].innerHTML = "X";
+  a++;
 
-if(a%2 === 0){		
-second[0].style.height ="0px"		
-second[0].style.overflow ="hidden"		
-myham[0].innerHTML ="&#9776"
-reg1[0].style.opacity ="1";
-		}
- }
-
+  if (a % 2 === 0) {
+    second[0].style.height = "0px";
+    second[0].style.overflow = "hidden";
+    myham[0].innerHTML = "&#9776";
+    reg1[0].style.opacity = "1";
+  }
+}
