079eafdfedb4abcc9ed25faa7f008ec34df88721
Mustapha Moshood <olawale3415@gmail.com>
2020-06-01 18:35:34 +0100

    test created

--- olawale1rty/server.js
+++ olawale1rty/server.js
@@ -1,6 +1,7 @@
 const config = require("config");
 const express = require('express');
 const app = express();
+module.exports = app.listen(3000);
 const cors = require('cors');
 app.use(express.json());
 app.use(express.urlencoded({exteneded:true}));
@@ -40,9 +41,9 @@ app.get('/logs',(req, res)=>{
 app.use(express.static(path.join(__dirname,'/public')));
 
 //welcome page
-app.get(['/','/index.html'],(req, res)=>{
-	res.sendFile(path.join(__dirname,'/public'));
-})
+// app.get(['/','/index.html'],(req, res)=>{
+// 	res.sendFile(path.join(__dirname,'/public'));
+// })
 
 
 
