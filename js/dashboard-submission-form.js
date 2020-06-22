1b032505fc86a83d9223409253d3ebda08f2ad4d
coded1guy <grggmmnl@gmail.com>
2020-06-22 13:34:24 +0100

    Update dashboard-submission-form.js

--- js/dashboard-submission-form.js
+++ js/dashboard-submission-form.js
@@ -130,3 +130,87 @@ projectImage.addEventListener( 'click' , function show() {
     vR7.style.visibility = 'hidden';
     vR8.style.visibility = 'visible';
 });
+
+/*
+if projectName.length
+var projectTagline = document.getElementById('projectTagline');
+var projectUSe = document.getElementById('projectUSe');
+var projectChallenge = document.getElementById('projectChallenge');
+var projectTechnology = document.getElementById('projectTechnology');
+var projectLink = document.getElementById('projectLink');
+var projectVideoLink = document.getElementById('projectVideoLink');
+var projectImage = document.getElementById('projectImage');*/
+
+
+// THE INPUT LENGTH COUNTER
+function countInput1(str) {
+    var lng = str.length;
+    document.getElementById('inputCount1').innerHTML =lng + ' / 50';
+}
+function countInput2(str) {
+    var lng = str.length;
+    document.getElementById('inputCount2').innerHTML =lng + ' / 200';
+}
+function countInput3(str) {
+    var lng = str.length;
+    document.getElementById('inputCount3').innerHTML =lng + ' / 200';
+}
+function countInput4(str) {
+    var lng = str.length;
+    document.getElementById('inputCount4').innerHTML =lng + ' / 200';
+}
+function countInput5(str) {
+    var lng = str.length;
+    document.getElementById('inputCount5').innerHTML =lng + ' / 200';
+}
+function countInput6(str) {
+    var lng = str.length;
+    document.getElementById('inputCount6').innerHTML =lng + ' / 50';
+}
+function countInput7(str) {
+    var lng = str.length;
+    document.getElementById('inputCount7').innerHTML =lng + ' / 50';
+}
+
+
+// THE INPUT FILE(IMAGE) BEHAVIOR
+
+
+
+function readFile() {
+    var reader = new FileReaader();
+    var file = document.getElementById('demo').files[0];
+    reader.onload = function(e) {
+        document.getElementById('result').src = e.target.result;
+    };
+    reader.readAsDataURL(file);
+}
+
+
+
+function FileDetails() { 
+    // GET THE FILE INPUT.
+    var fi = document.getElementById('file'); 
+    // VALIDATE OR CHECK IF ANY FILE IS SELECTED. 
+    if (fi.files.length > 0) { 
+    // THE TOTAL FILE COUNT.
+    document.getElementById('fp').innerHTML = 'Total Files: <b>' + fi.files.length + '</b></br >';
+     // RUN A LOOP TO CHECK EACH SELECTED FILE.
+      for (var i = 0; i <= fi.files.length - 1; i++) { var fname = fi.files.item(i).name; 
+      // THE NAME OF THE FILE. 
+      var fsize = fi.files.item(i).size; 
+      // THE SIZE OF THE FILE.
+       // SHOW THE EXTRACTED DETAILS ""OF THE FILE.
+       document.getElementById('fp').innerHTML = document.getElementById('fp').innerHTML + '<br /> ' + fname + ' (<b>' + fsize + '</b> bytes)'; } } else { alert('Please select a file.') } }
+       function readURL(input) {
+                if (input.files && input.files[0]) {
+                    var reader = new FileReader();
+    
+                    reader.onload = function (e) {
+                        $('#blah')
+                            .attr('src', e.target.result);
+                    };
+    
+                    reader.readAsDataURL(input.files[0]);
+                }
+            } 
