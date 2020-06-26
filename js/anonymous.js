313fd35310134746b72d08d5ec249df3c75e8b75
fasasisherif <fasasisherif@gmail.com>
2020-06-27 00:56:29 +0100

    dashboard-annonymous

---
+++ js/anonymous.js
@@ -0,0 +1,12 @@
+let dashboard_x = document.getElementsByClassName("dashboard_x")
+let dashboard_cancel = document.getElementsByClassName("dashboard_cancel")
+let dashboard_anonymous = document.getElementsByClassName("dashboard_anonymous")
+let anonymous_pic = document.getElementsByClassName("anonymous_pic")
+
+
+dashboard_x[0].addEventListener("click", dashboard)		
+
+function dashboard() {
+dashboard_cancel[0].style.display ="none"
+dashboard_anonymous[0].style.display ="none"
+		}
