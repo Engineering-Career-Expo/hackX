86544597f36b93d7ecf064a25fb4844e4aa7924f
coded1guy <grggmmnl@gmail.com>
2020-06-30 14:51:40 +0100

    Update Dashboard2-section.js

--- js/Dashboard2-section.js
+++ js/Dashboard2-section.js
@@ -1,3 +1,22 @@
+window.addEventListener('load', function start() {
+    var countDownDate = new Date("June 23, 2021 17:35:00").getTime();
+
+    var x = setInterval(function() {
+        var todayDate = new Date().getTime();
+        var distance = countDownDate - todayDate;
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        document.querySelector('#sub_section__timeLeft').innerHTML = days + "d:" + hours + "h:" + minutes + "m";
+        
+        if (distance < 0) {
+            clearInterval(x);
+            document.querySelector('#sub_section__timeLeft').innerHTML = "EXPIRED";
+        }
+    }, 1000);   
+});
+
+
 // THE BUTTONS ID
 var timelineBtn = document.getElementById('timelineBtn');
 var announBtn = document.getElementById('announBtn');
@@ -102,19 +121,3 @@ overviewBtn.addEventListener('click', function open() {
     pl.style.display = 'none';
     ol.style.display = 'block';
 });
-
-var countDownDate = new Date("June 23, 2021 17:35:00").getTime();
-
-var x = setInterval(function() {
-	var todayDate = new Date().getTime();
-	var distance = countDownDate - todayDate;
-	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-	document.querySelector('#sub_section__timeLeft').innerHTML = days + "d:" + hours + "h:" + minutes + "m";
-	
-	if (distance < 0) {
-		clearInterval(x);
-		document.querySelector('#sub_section__timeLeft').innerHTML = "EXPIRED";
-	}
-}, 1000);
