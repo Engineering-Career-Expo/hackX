2d5d2cfe95d5f36aea0edbd0e3af9bee1a72e507
Mustapha Moshood <olawale3415@gmail.com>
2020-07-18 05:33:25 +0100

    fixed some errors

--- js/forgot-password.js
+++ js/forgot-password.js
@@ -18,7 +18,7 @@ const formEvent = loginForm.addEventListener("submit", async (event) => {
   localStorage.setItem("email", email);
   const username = document.querySelector("#username").value;
   const forgotPasswordInfo = await { email, username };
-  if (email === "" || password === "") {
+  if (email === "" || username === "") {
     showLoginAlert("Please Fill All Fields", "error");
   } else {
     userDetails(forgotPasswordInfo);
@@ -30,11 +30,11 @@ const userDetails = async (forgotPasswordInfo) => {
   headers = {
     'Content-Type': 'application/json',
     'withCredentials': true
-  }
+  };
 
 
   axios
-    .post("https://hackxbackend.herokuapp.com/forgotPassword/",  forgotPasswordInfo, { headers: headers})
+    .post("https://hackxbackend.herokuapp.com/forgotPassword",  forgotPasswordInfo, { headers: headers})
     .then((response) => {
       response.data;
       // console.log(response)
