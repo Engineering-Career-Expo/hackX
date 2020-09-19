811c0803afdff555831bb34a042029d2b3fb80bf
Olubiyi Emmanuel <42422184+coded1guy@users.noreply.github.com>
2020-09-20 00:00:18 +0100

    Update admin-submission-inbox.js

--- js/admin-submission-inbox.js
+++ js/admin-submission-inbox.js
@@ -1,44 +1,35 @@
 // GETTING THE DATA FROM THE BACKEND
+let applicont = document.querySelector('#appliContainer');
 const headers = {
     'Content-Type': 'application/json',
     'Authorization': "Bearer" + ' ' + localStorage.getItem("pass"),
     'withCredentials': true, 
   }
   
-  axios.get("https://hackxbackend.herokuapp.com/alluser" , { headers: headers })
-  .then((response) => {
-    //   console.log(response.data); 
-      var doc = response.data.doc;
-      for(var i = 0; i < doc.length; i++) {
-          var newL = JSON.stringify(doc[i]) + "<br><br><br><br>";
-          document.body.innerHTML += newL;
-          console.log(doc[i].firstname);
-      }
-  })
-  .catch((error) => console.error(error));
+axios.get("https://hackxbackend.herokuapp.com/alluser" , { headers: headers })
+.then((response) => {
+  var doc = response.data.doc;
+  console.log(doc); 
+  let newCont = () => {
+      let newPane = 
+      `<div class="submission-detail">
+          <input type="checkbox">
+          <div class="column-one">
+              <div class="checkbox" id="checkbox-one"></div>
+              <div class="real-submission">
+                  <h5 class="submission-tag">Application ${(i + 1)}</h5>
+                  <h5 class="participant-name">${doc[i].firstname + " "} ${doc[i].lastname}</h5>
+              </div>
+          </div>
+          <h5 class="time">${doc[i].createdAt}</h5>
+      </div>
+      <hr class="submission-hr"></hr>`;
+      applicont.innerHTML += newPane;
+  }
+  for(var i = 0; i < doc.length; i++) {
+      newCont();
+      console.log('boo');
 
-
-  // var doc = response.data.doc;
-  //   console.log(doc); 
-  //   let newCont = () => {
-  //       let newPane = 
-  //       `<div class="submission-detail">
-  //           <input type="checkbox">
-  //           <div class="column-one">
-  //               <div class="checkbox" id="checkbox-one"></div>
-  //               <div class="real-submission">
-  //                   <h5 class="submission-tag">Application ${(i + 1)}</h5>
-  //                   <h5 class="participant-name">${doc[i].name}</h5>
-  //               </div>
-  //           </div>
-  //           <h5 class="time">${doc[i].createdAt}</h5>
-  //       </div>
-  //       <hr class="submission-hr"></hr>`;
-  //       applicont.innerHTML += newPane;
-  //   }
-  //   for(var i = 0; i < doc.length; i++) {
-  //       var bigDocument = JSON.stringify(doc[i]) + "<br><br><br><br>";
-  //       newCont();
-  //       console.log('boo');
-
-  //   }
+  }
+})
+.catch((error) => console.error(error));
