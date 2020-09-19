faea9d12380a0fbc0d8996ec55d750f2d70cddac
Olubiyi Emmanuel <42422184+coded1guy@users.noreply.github.com>
2020-09-19 21:39:26 +0100

    Update admin-submission-inbox.js

--- js/admin-submission-inbox.js
+++ js/admin-submission-inbox.js
@@ -16,3 +16,29 @@ const headers = {
       }
   })
   .catch((error) => console.error(error));
+
+
+  // var doc = response.data.doc;
+  //   console.log(doc); 
+  //   let newCont = () => {
+  //       let newPane = 
+  //       `<div class="submission-detail">
+  //           <input type="checkbox">
+  //           <div class="column-one">
+  //               <div class="checkbox" id="checkbox-one"></div>
+  //               <div class="real-submission">
+  //                   <h5 class="submission-tag">Application ${(i + 1)}</h5>
+  //                   <h5 class="participant-name">${doc[i].name}</h5>
+  //               </div>
+  //           </div>
+  //           <h5 class="time">${doc[i].createdAt}</h5>
+  //       </div>
+  //       <hr class="submission-hr"></hr>`;
+  //       applicont.innerHTML += newPane;
+  //   }
+  //   for(var i = 0; i < doc.length; i++) {
+  //       var bigDocument = JSON.stringify(doc[i]) + "<br><br><br><br>";
+  //       newCont();
+  //       console.log('boo');
+
+  //   }
