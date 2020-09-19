c07bc2d936142e1aeadea40cbff086eadf2ce2a2
Olubiyi Emmanuel <42422184+coded1guy@users.noreply.github.com>
2020-09-19 21:39:05 +0100

    Update all-applicants.js

--- js/all-applicants.js
+++ js/all-applicants.js
@@ -7,24 +7,27 @@ const headers = {
   
 axios.get("https://hackxbackend.herokuapp.com/getAllContacts" , { headers: headers })
 .then((response) => {
-    console.log(response.data); 
     var doc = response.data.doc;
-    // let newPane = 
-    // `<div class="submission-detail">
-    //     <input type="checkbox">
-    //     <div class="column-one">
-    //         <div class="checkbox" id="checkbox-one"></div>
-    //         <div class="real-submission">
-    //             <h5 class="submission-tag">Application ${(doc[i] + 1)}</h5>
-    //             <h5 class="participant-name">${doc[i].name}</h5>
-    //         </div>
-    //     </div>
-    //     <h5 class="time">${doc[i].createdAt}</h5>
-    // </div>
-    // <hr class="submission-hr"></hr>`;
+    console.log(doc); 
+    let newCont = () => {
+        let newPane = 
+        `<div class="submission-detail">
+            <input type="checkbox">
+            <div class="column-one">
+                <div class="checkbox" id="checkbox-one"></div>
+                <div class="real-submission">
+                    <h5 class="submission-tag">Application ${(i + 1)}</h5>
+                    <h5 class="participant-name">${doc[i].name}</h5>
+                </div>
+            </div>
+            <h5 class="time">${doc[i].createdAt}</h5>
+        </div>
+        <hr class="submission-hr"></hr>`;
+        applicont.innerHTML += newPane;
+    }
     for(var i = 0; i < doc.length; i++) {
         var bigDocument = JSON.stringify(doc[i]) + "<br><br><br><br>";
-        applicont.innerHTML += bigDocument;
+        newCont();
         console.log('boo');
 
     }
