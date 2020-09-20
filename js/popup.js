3bb31ee7598b818f11c0f26c22ae8232b5b65cd1
Olubiyi Emmanuel <42422184+coded1guy@users.noreply.github.com>
2020-09-20 21:17:12 +0100

    Update popup.js

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,13 @@
-var closePopUp = document.querySelector('#close_image');
-var welcomePopUp = document.querySelector('.main_dashboard__welcome');
-var allPage = document.querySelector('.main_dashboard__pageAll');
-allPage.classList.add('stop_scroll');
-closePopUp.addEventListener('click', function() {
-	welcomePopUp.style.display = "none";
-	allPage.classList.remove('stop_scroll');
-});
+let popUp = document.querySelector('#close-cont');
+let poppy = localStorage.getItem("PaseenPopup");
+if(poppy == "checked") {
+    popUp.style.display = "none";
+}
+let closeBtn = document.querySelector('#close_image');
+closeBtn.onclick = function() {
+    popUp.style.transition = '.5s ease-in-out';
+    popUp.style.opacity = '0';
+    popUp.style.display = 'none';
+    localStorage.setItem("PaseenPopup", "checked");
+    console.log('close');
+}
