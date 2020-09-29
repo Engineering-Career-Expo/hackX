0e4e51f542ccfb2ed810dec0c357fc525593cd10
Olubiyi Emmanuel <42422184+coded1guy@users.noreply.github.com>
2020-09-29 19:31:29 +0100

    hnh

--- js/add_link.js
+++
@@ -1,9 +0,0 @@
-var linkk = document.querySelector("#link");
-var newProfile = document.querySelector(".new-profile");
-newProfile.addEventListener('click', () => {
-	if (linkk.value.length == 0) {
-		showLoginAlert('Link cannot be empty', 'error');
-	}else{
-		showLoginAlert('Link updated', 'success');
-	}
-});
